48a3565b5a9bc401b7f98ffd152eaa3413c2c492
AndresC <andrescamachoclaros@gmail.com>
2021-06-27 21:49:02 -0500

    foro completo junto con el frontend, aun falta las tematicas

--- database/configDB.js
+++ database/configDB.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 
 const db = new Sequelize(process.env.DBNOMBRE, process.env.DBUSUARIO, process.env.DBPASSWORD, {
-    host:'localhost',
+    host: process.env.DBHOST,
     dialect:'postgres'
 
 });
