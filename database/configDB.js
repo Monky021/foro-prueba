c836b8f43300c7af244da64f1cd74a7f5484423d
AndresC <andrescamachoclaros@gmail.com>
2021-06-27 22:29:04 -0500

    base de datos conectada en heroku

--- database/configDB.js
+++ database/configDB.js
@@ -2,10 +2,19 @@ const { Sequelize } = require('sequelize');
 
 
 const db = new Sequelize(process.env.DBNOMBRE, process.env.DBUSUARIO, process.env.DBPASSWORD, {
-    host: process.env.DBHOST,
-    dialect:'postgres'
+    host: process.env.DBHOST || 'localhost',
+    dialect:'postgres',
+    dialectOptions:{
+        ssl:{
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 
 });
+console.log(process.env.DBNOMBRE);
+
+//const db = new Sequelize(process.env.DATABASE_URL);
 
 
 module.exports= db;
